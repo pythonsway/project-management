f08b6e16da0b302742b3bd18e3a34b14f57eafc5
Pythonsway <adam@pythonsway.it>
2022-09-12 21:45:37 +0200

    deploy ready

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const colors = require('colors');
 const cors = require('cors');
+const path = require('path');
 
 const connectDB = require('./config/db');
 const schema = require('./schema/schema');
@@ -14,9 +15,15 @@ connectDB();
 
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: process.env.NODE_ENV === 'development'
 }));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
 app.listen(port, console.log(`running at ${port}`));
